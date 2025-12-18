0202665415808158670f1c1365fb0966f50d903e
kanijr <zitfitgpo@gmail.com>
2025-12-18 17:01:58 +0200

    Delet unused code

--- api/src/db/index.js
+++ api/src/db/index.js
@@ -1,14 +1,5 @@
 import { Pool } from "pg";
 
-// const createClient = () =>
-//   new Client({
-//     user: "postgres",
-//     host: "localhost",
-//     database: "shop_normalization",
-//     password: "root",
-//     port: 5432,
-//   });
-
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
